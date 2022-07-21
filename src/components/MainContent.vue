06711539d3f9049fd4a418f8dbab58be698cbf64
Antonius131 <antomendetta@gmail.com>
2022-07-21 12:56:07 +0200

    Creating components and put them in App.vue

---
+++ src/components/MainContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <MoviesList />
+</template>
+
+<script>
+import MoviesList from './MoviesList.vue';
+export default {
+   components: { 
+      MoviesList 
+   }
+}
+</script>
+
+<style>
+
+</style>
