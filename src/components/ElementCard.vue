1581d906ad4f996ed8ba28bc2fda5fc67547d39b
Antonius131 <antomendetta@gmail.com>
2022-07-21 15:18:08 +0200

    create ElementCard component

---
+++ src/components/ElementCard.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="card">
+      <h1>Titolo</h1>
+      <h3>Titolo originale</h3>
+      <p>Lingua</p>
+      <pre>Voto</pre>
+   </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
