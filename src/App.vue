95111c9cd610ff3dd7ca793904ad32b9017a8871
Antonius131 <antomendetta@gmail.com>
2022-07-21 18:02:15 +0200

    App: changed URL for multi request

--- src/App.vue
+++ src/App.vue
@@ -20,26 +20,16 @@ export default {
   data: function() {
     return {
       moviesList: [],
-      movieUrl: 'https://api.themoviedb.org/3/search/movie?api_key=e575f76f59f9dd45a7033ae7e19eb74d&include_adult=false&query=',
-      tvShowsUrl: 'https://api.themoviedb.org/3/search/tv?api_key=e575f76f59f9dd45a7033ae7e19eb74d&linclude_adult=false&query='
+      multiSearchUrl: 'https://api.themoviedb.org/3/search/multi?include_adult=false&api_key=e575f76f59f9dd45a7033ae7e19eb74d&query=',
     }
   },
   methods: {
     getSearch(value) {
       const queryValue = value;
-      let searchedMovies;
-      let searchedSeries;
-
-      axios.get(`${this.movieUrl}${queryValue}`)
+      axios.get(`${this.multiSearchUrl}${queryValue}`)
       .then((result) => {
-        searchedMovies = result.data.results;
-        console.log(searchedMovies);
-      });
-
-      axios.get(`${this.tvShowsUrl}${queryValue}`)
-      .then((result) => {
-        searchedSeries = result.data.results;
-        console.log(searchedSeries);
+        this.moviesList = result.data.results;
+        console.log(this.moviesList);
       });
     }
   }
