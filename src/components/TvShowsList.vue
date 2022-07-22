547905c92433eb203fd37f1719e7cf7af25f6e64
Antonius131 <antomendetta@gmail.com>
2022-07-22 08:25:00 +0200

    TvShowsList: populate template with props

--- src/components/TvShowsList.vue
+++ src/components/TvShowsList.vue
@@ -1,10 +1,30 @@
 <template>
    <div class="card">
+    <ul>
+      <li>{{ showTitle }}</li>
+      <li>{{ showOriginalTitle }}</li>
+      <div class="lang-img" v-if="showLanguage === 'it'">
+        <img src="../assets/img/it-flag.png" :alt="showLanguage">
+      </div>
+      <div class="lang-img" v-else-if="showLanguage === 'en'">
+        <img src="../assets/img/uk-flag.png" :alt="showLanguage">
+      </div>
+      <div class="lang-img" v-else>
+        <img src="" alt="no-flag">
+      </div>
+      <li>{{ showVote }}</li>
+    </ul>
    </div>
 </template>
 
 <script>
 export default {
+   props: {
+      'showTitle': String,
+      'showOriginalTitle': String,
+      'showLanguage': String,
+      'showVote': Number
+   }
 }
 </script>
 
