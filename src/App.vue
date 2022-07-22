b76200a801c78e383fa7ef4d3f95fe359187b6b3
Antonius131 <antomendetta@gmail.com>
2022-07-22 08:20:35 +0200

    App: making two API requests

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <MainHeader @search="getSearch"/>
     <MainContent 
-      :movies="moviesList"/>
+      :movies="moviesList"
+      :tvShows="tvShowsList"/>
   </div>
 </template>
 
@@ -20,17 +21,25 @@ export default {
   data: function() {
     return {
       moviesList: [],
-      multiSearchUrl: 'https://api.themoviedb.org/3/search/multi?include_adult=false&api_key=e575f76f59f9dd45a7033ae7e19eb74d&query=',
+      tvShowsList: [],
+      movieUrl: 'https://api.themoviedb.org/3/search/movie?api_key=e575f76f59f9dd45a7033ae7e19eb74d&include_adult=false&query=',
+      tvShowsUrl: 'https://api.themoviedb.org/3/search/tv?api_key=e575f76f59f9dd45a7033ae7e19eb74d&linclude_adult=false&query='
     }
   },
   methods: {
     getSearch(value) {
       const queryValue = value;
-      axios.get(`${this.multiSearchUrl}${queryValue}`)
+      axios.get(`${this.movieUrl}${queryValue}`)
       .then((result) => {
         this.moviesList = result.data.results;
         console.log(this.moviesList);
       });
+
+      axios.get(`${this.tvShowsUrl}${queryValue}`)
+      .then((result) => {
+        this.tvShowsList = result.data.results;
+        console.log(this.tvShowsList);
+      })
     }
   }
 }
