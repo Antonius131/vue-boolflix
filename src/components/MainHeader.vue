deccb9f4bdba9355e2313ba2fea725c0e40f4691
Antonius131 <antomendetta@gmail.com>
2022-07-22 14:13:31 +0200

    MainHeader styling

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,9 +1,14 @@
 <template>
    <header>
-      <div class="user-search">
-         <input class="search-bar" placeholder="Ricerca" v-model="searchValue" >
-         <button class="btn" type="submit" @click="$emit('search', searchValue)">Cerca</button>
-      </div>
+      <nav>
+         <div class="logo-box">
+            <h3>Boolflix</h3>
+         </div>
+         <div class="user-search">
+            <input class="search-bar" placeholder="Ricerca" v-model="searchValue" >
+            <button class="nav-btn" type="submit" @click="$emit('search', searchValue)">Cerca</button>
+         </div>
+      </nav>
    </header>
 </template>
 
@@ -17,6 +22,41 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+   header {
+      background-color: rgb(24, 24, 24);
+      padding: 1rem 4rem;
+
+      nav {
+         display: flex;
+         justify-content: space-between;
+         align-items: center;
+
+         h3 {
+            color: #C00;
+            font-weight: 800;
+         }
+
+         .search-bar {
+            padding: .45rem .75rem;
+            margin-right: .5rem;
+            border: none;
+         }
+
+         .nav-btn {
+            padding: .45rem 1rem;
+            border: none;
+            border-radius: 0px;
+            background-color: #C00;
+            color: #fff;
+            font-weight: 600;
+
+            &:hover {
+               cursor: pointer;
+               background-color: #D00;
+            }
+         }
+      }
+   }
 
 </style>
